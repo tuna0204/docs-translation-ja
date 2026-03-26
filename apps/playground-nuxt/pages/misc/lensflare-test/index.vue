c5e37e729a782d4ee35fe1c563e649e53e55a24d
alvarosabu <alvaro.saburido@gmail.com>
2026-03-26 17:35:16 +0100

    refactor(lensflare): remove size property from controls in Lensflare component and demo

--- apps/playground-nuxt/pages/misc/lensflare-test/index.vue
+++ apps/playground-nuxt/pages/misc/lensflare-test/index.vue
@@ -3,12 +3,11 @@ import { Lensflare } from '@tresjs/cientos'
 const uuid = 'lensflare-test'
 const [x, z] = [shallowRef(0), shallowRef(0)]
 
-const { scale, seed, color, distance, size } = useControls({
+const { scale, seed, color, distance } = useControls({
     scale: { value: 0.33, min: 0.01, max: 2, step: 0.01 },
     seed: { value: 1028, min: 1, max: 5000, step: 1 },
     color: '#ffffff',
     distance: { value: 0, min: -2, max: 2, step: 0.1 },
-    size: { value: 2, min: 0.1, max: 10, step: 0.5 },
 }, { uuid })
 
 function onLoop({ elapsed }: { elapsed: number }) {
@@ -22,7 +21,7 @@ function onLoop({ elapsed }: { elapsed: number }) {
     <TresCanvas clear-color="#333" @loop="onLoop">
         <OrbitControls />
         <TresPointLight :position="[x, 0, z]">
-            <Lensflare :seed="seed" :scale="scale" :color="color" :distance="distance" :size="size" />
+            <Lensflare :seed="seed" :scale="scale" :color="color" :distance="distance" />
         </TresPointLight>
         <Torus v-for="n in [-2, 0, 2]" :key="n" :args="[0.7, 0.15]" :position-z="n" :rotation-y="Math.PI * 0.5">
             <TresMeshPhongMaterial color="#888" />
